d804885838ba5248a19578af9a38054cdf38dffd
Your Name <you@example.com>
2025-05-20 19:56:07 +0300

    Sudoku

--- main/server.js
+++ main/server.js
@@ -2339,7 +2339,7 @@ app.post("/game/move", isUser, async (req, res) => {
 
 		// Сохранение изменений
 		await userInfo.save({ transaction });
-		game.grid = [...game.grid];
+		game.grid = JSON.parse(JSON.stringify(updatedGrid));
 		await game.save({ transaction });
 		await transaction.commit();
 
